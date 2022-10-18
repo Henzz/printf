7546b6829121b2288e7a7830443e8caa4615015a
Henzz <khenok53@gmail.com>
2022-10-18 15:14:38 -0700

    fixed betty style of main.h

--- main.h
+++ main.h
@@ -21,7 +21,8 @@ typedef struct print
 } print_t;
 
 int _printf(const char *format, ...);
-int ev_print_func(const char *s, int index); /* Struct array of data identifiers and functions*/
+/* Struct array of data identifiers and functions*/
+int ev_print_func(const char *s, int index);
 int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
 unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
 int print_buf(char *buf, unsigned int nbuf);
@@ -54,7 +55,7 @@ int prinnoct(va_list arguments, char *buf, unsigned int ibuf);
 int prinnhex(va_list arguments, char *buf, unsigned int ibuf);
 int prinnupx(va_list arguments, char *buf, unsigned int ibuf);
 int prinsint(va_list arguments, char *buf, unsigned int ibuf);
-char *fill_binary_array(char *binary, long int int_in, int isnegative, int limit);
+char *fill_binary_array(char *binary, long int int_in, int isneg, int limit);
 char *fill_oct_array(char *binary, char *oct);
 char *fill_long_oct_array(char *bnr, char *oct);
 char *fill_short_oct_array(char *bnr, char *oct);
